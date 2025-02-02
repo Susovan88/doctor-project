5cd16604d9fce2722a0b206ca9cc5596cdfd5ac0
Susovan Paul <palsusovan88@gmail.com>
2025-02-02 09:44:43 +0530

    ass

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -4,6 +4,7 @@ import { useAppContext } from '../context/AppContext';
 
 function Doctors() {
   const [filterDoctors,setFilterDoctors]=useState([]);
+  const [showFilter,setShowFilter]=useState(false);
   const navigate=useNavigate();
   const {speciality}=useParams();
   const {doctors}=useAppContext();
@@ -28,8 +29,9 @@ function Doctors() {
   return (
     <div>
       <p className='text-gary-600'>Browse through the doctors specialist.</p>
-      <div className='flex flex-col sm:flex-row items-startgap-5 mt-5'>
-        <div className='flex flex-col gap-4 textsm text-gary-600'>
+      <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
+        <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter? "bg-primary text-white" : ""}`} onClick={()=>setShowFilter(prev=>!prev)}>Filters</button>
+        <div className={` flex-col gap-4 textsm text-gary-600 ${showFilter? "flex" : "hidden sm:flex" }`}>
         <p onClick={()=>{haldleSpeciality('All Doctors')}} className={`w-[94vw] sm:w-auto pl-3 pr-16 py-1.5 border border-gray-300 rounded transition-all cursor-pointer ${ !speciality ? 'bg-indigo-100' : ''}`}>All Doctors</p>
           <p onClick={()=>{haldleSpeciality('General physician')}} className={`w-[94vw] sm:w-auto pl-3 pr-16 py-1.5 border border-gray-300 rounded transition-all cursor-pointer ${speciality==='General physician'? 'bg-indigo-100' : ''} `}>General physician</p>
           <p onClick={()=>{haldleSpeciality('Gynecologist')}} className={`w-[94vw] sm:w-auto pl-3 pr-16 py-1.5 border border-gray-300 rounded transition-all cursor-pointer ${speciality==='Gynecologist'? 'bg-indigo-100' : ''}`}>Gynecologist</p>
